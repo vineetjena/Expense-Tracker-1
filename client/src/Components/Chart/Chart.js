019ff6afdb30181da75f41b0954a33f9395fa0cf
vineetjena <136954835+vineetjena@users.noreply.github.com>
2025-06-20 22:39:04 +0530

    Update Chart.js

--- client/src/Components/Chart/Chart.js
+++ client/src/Components/Chart/Chart.js
@@ -26,16 +26,49 @@ ChartJs.register(
     ArcElement,
 )
 
-
 function Chart() {
-  return (
-    <ChartStyled>
-        <Line />
-    </ChartStyled>
-  )
+    const {incomes, expenses} = useGlobalContext()
+
+    const data = {
+        labels: incomes.map((inc) =>{
+            const {date} = inc
+            return dateFormat(date)
+        }),
+        datasets: [
+            {
+                label: 'Income',
+                data: [
+                    ...incomes.map((income) => {
+                        const {amount} = income
+                        return amount
+                    })
+                ],
+                backgroundColor: 'green',
+                tension: .2
+            },
+            {
+                label: 'Expenses',
+                data: [
+                    ...expenses.map((expense) => {
+                        const {amount} = expense
+                        return amount
+                    })
+                ],
+                backgroundColor: 'red',
+                tension: .2
+            }
+        ]
+    }
+
+
+    return (
+        <ChartStyled >
+            <Line data={data} />
+        </ChartStyled>
+    )
 }
 
-const ChartStyled=styled.div`
+const ChartStyled = styled.div`
     background: #FCF6F9;
     border: 2px solid #FFFFFF;
     box-shadow: 0px 1px 15px rgba(0, 0, 0, 0.06);
